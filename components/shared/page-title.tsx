6dde5df2f61e339f512c4fa0c9c9907d8e7cccc9
felixesteban100 <felixesteban100@gmail.com>
2024-03-15 19:40:42 -0400

    building error of vercel fixed about key in PageTitle otherChildren props

--- components/shared/page-title.tsx
+++ components/shared/page-title.tsx
@@ -1,14 +1,20 @@
 
 type PageTitleProps = {
     title: string;
-    otherChildren?: JSX.Element | JSX.Element[]
+    otherChildren?: JSX.Element[]
 }
 
 export default function PageTitle({ title, otherChildren }: PageTitleProps) {
     return (
         <div className="flex flex-col lg:flex-row gap-5 lg:gap-0 justify-between items-center mb-8">
             <h1 className="text-4xl font-bold">{title}</h1>
-            {otherChildren}
+            {otherChildren?.map((child: JSX.Element, index: number) => {
+                return (
+                    <div key={index}>
+                        {child}
+                    </div>
+                )
+            })}
         </div>
     )
 }
